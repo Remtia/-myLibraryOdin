96e2835e92645cf4914ee9f3ae342b590ae6d3ee
Remtia <224189853+Remtia@users.noreply.github.com>
2026-01-03 23:17:10 +0000

    book object constructor

---
+++ script.js
@@ -0,0 +1,13 @@
+const button = document.querySelector('button');
+const bookshelf = document.querySelector('.bookshelf');
+
+function Book (title, author, pages, hasRead) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.hasRead = hasRead;
+}
+
+function addBook (title, author, pages, hasRead) {
+    
+}
